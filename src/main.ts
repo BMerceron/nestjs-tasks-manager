91b7ff1bc97ff9dded006e85e5db8aec6eb15450
Bastien Merceron <bastien.merceron@conserto.pro>
2023-09-20 09:21:21 +0200

    update router + add alert component

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+  // Enable CORS
+  app.enableCors();
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+    }),
+  );
   const port = '3000';
   Logger.log(`Application is running on port : ${port}`);
   await app.listen(port);
